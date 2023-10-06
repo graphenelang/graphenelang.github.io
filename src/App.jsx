8498dc0a6a9e06525cfd8b613932492962468c1e
Devin Rockwell <zimlit2010@gmail.com>
2023-10-06 13:52:08 -0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <div class={styles.header}>
+        <h1>Graphene</h1>
+        <p>A statically typed, compiled, data oriented language.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
